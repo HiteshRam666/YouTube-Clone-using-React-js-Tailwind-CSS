d5c9ad2e6eb1f7af134807cd838c1ffdfd08a543
Hitesh Ram <hiteshram321@gmail.com>
2023-09-12 23:00:17 +0530

    feed created

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "c06648ed29msh406c3f76770e3a6p10a21djsncaa31eabe0a1",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+}; 
